c4d8e955d58805e97365a46e6ce99ef6cf665ee3
Tales <tales.dk@hotmail.com>
2024-11-12 07:44:48 -0300

    Primeiro commit

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { MaterialProvider } from './context/MaterialContext';
+
+import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
+import RegistroVendaScreen from './screens/RegistroVendaScreen';
+import AvaliacaoEstoqueScreen from './screens/AvaliacaoEstoqueScreen';
+import RecebimentoMercadoriaScreen from './screens/RecebimentoMercadoriaScreen';
+import FaturamentoScreen from './screens/FaturamentoScreen';
+import CadastroMaterialScreen from './screens/CadastroMaterialScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <MaterialProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen 
+            name="Login" 
+            component={LoginScreen} 
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen 
+            name="Home" 
+            component={HomeScreen} 
+            options={{ title: 'Menu Principal' }}
+          />
+          <Stack.Screen 
+            name="RegistroVenda" 
+            component={RegistroVendaScreen} 
+            options={{ title: 'Registro de Venda' }}
+          />
+          <Stack.Screen 
+            name="AvaliacaoEstoque" 
+            component={AvaliacaoEstoqueScreen} 
+            options={{ title: 'Avaliação do Estoque' }}
+          />
+          <Stack.Screen 
+            name="RecebimentoMercadoria" 
+            component={RecebimentoMercadoriaScreen} 
+            options={{ title: 'Recebimento de Mercadoria' }}
+          />
+          <Stack.Screen 
+            name="Faturamento" 
+            component={FaturamentoScreen} 
+            options={{ title: 'Faturamento' }}
+          />
+          <Stack.Screen 
+            name="CadastroMaterial" 
+            component={CadastroMaterialScreen} 
+            options={{ title: 'Cadastro de Produto' }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </MaterialProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
